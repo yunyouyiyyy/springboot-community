76113e11282c120726c12c6f7994350786b44670
yunyouyiyyy <593865332@qq.com>
2024-12-05 12:08:36 +0800

    传统JDBC连接

--- src/main/java/org/example/spring1114/service/UserService.java
+++ src/main/java/org/example/spring1114/service/UserService.java
@@ -1,41 +1,28 @@
+
 package org.example.spring1114.service;
 
-import org.example.spring1114.dao.UserDAO;
+import org.example.spring1114.dao.IUserDAO;
 import org.example.spring1114.event.RegEvent;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService implements IUserService {
+public class UserService {
 
-    // 注入 ApplicationEventPublisher
-    private final ApplicationEventPublisher eventPublisher;
-
-    public UserService(ApplicationEventPublisher eventPublisher){
-        this.eventPublisher = eventPublisher;
-    };
-
-    private UserDAO userDAO = new UserDAO();
-
-
+    private final IUserDAO userDao;
+    public UserService(@Qualifier("jdbcUserDao") IUserDAO userDao) {
+        this.userDao = userDao;
+    }
+    public void register(String username, String password,String email) {
+        if (!userDao.userExists(username)) {
+            userDao.addUser(username, password);
+        } else {
+            throw new IllegalArgumentException("⽤户已存在");
+        }
+    }
     public boolean login(String username, String password) {
-        String pwd = userDAO.getPasswordByUsername(username);
+        String pwd = userDao.getPasswordByUsername(username);
         return pwd != null && pwd.equals(password);
     }
-
-    public void register(String username, String password,String email) {
-        if (userDAO.userExists(username)) {
-            throw new RuntimeException("用户名已存在");
-        } else {
-            userDAO.addUser(username, password);
-            // 模拟用户注册逻辑
-            System.out.println("User registered: " + email);
-
-            // 发布事件
-            eventPublisher.publishEvent(new RegEvent(this, email));
-        }
-        System.out.println("注册成功");
-    }
-
-
 }
